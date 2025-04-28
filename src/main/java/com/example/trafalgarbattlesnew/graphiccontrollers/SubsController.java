5d0c16116584fdb9c9ce50ca9d2793898c68a256
AndreaSavoi <andrea.savoi2002@gmail.com>
2025-04-28 13:30:14 +0200

    Fix

--- src/main/java/com/example/trafalgarbattlesnew/graphiccontrollers/SubsController.java
+++ src/main/java/com/example/trafalgarbattlesnew/graphiccontrollers/SubsController.java
@@ -48,49 +48,7 @@ public class SubsController implements Initializable {
         try {
             BeanTournList tL = new BeanTournList();
             new ApplicationControllerTournaments(tL, "sub", CurrentUser.getUser());
-            int count = tL.sno.size();
-
-            for (int i = 0; i < count; i++) {
-                Pane pane = new Pane();
-                pane.setPrefSize(1083, 150);
-                Label label = new Label();
-                String paneId = "Info" + tL.getSNO(i);
-                pane.setId(paneId);
-                pane.setOnMouseClicked(event -> {
-                    int infoN = Integer.parseInt(((Pane) event.getSource()).getId().replace("Info", ""));
-                    BeanCurrTourn tournament = BeanCurrTourn.getInstance();
-                    tournament.setSno(infoN);
-                    visualizer.sceneVisualizer("TournamentInfo.fxml", event);
-                });
-                HBox hbox = new HBox();
-                hbox.setPrefHeight(125);
-                hbox.setPrefWidth(1083);
-                hbox.setSpacing(50);
-                Image img = new Image(tL.getLogos(i));
-                ImageView logo = new ImageView(img);
-                logo.setPreserveRatio(true);
-                logo.setFitHeight(hbox.getPrefHeight());
-                String tabs = "\t\t\t\t";
-                label.setText(tL.getName(i) + tabs + tL.getNS(i) + "/" + tL.getNP(i) + tabs + tL.getDate(i) + tabs);
-                label.setFont(new Font("Century Gothic", 20));
-                label.setStyle("-fx-font-weight: bold italic");
-                label.setLayoutX(logo.getFitWidth() + 50);
-                label.setLayoutY(hbox.getHeight() / 2);
-                hbox.getChildren().addAll(logo, label);
-                BorderStroke bS = new BorderStroke(
-                        Color.BLACK,
-                        BorderStrokeStyle.SOLID,
-                        CornerRadii.EMPTY,
-                        BorderWidths.DEFAULT
-                );
-                Border border = new Border(bS);
-                pane.setBorder(border);
-                pane.getChildren().addAll(hbox);
-                hbox.setAlignment(Pos.CENTER);
-                hbox.setLayoutY((pane.getPrefHeight() - hbox.getPrefHeight()) / 2);
-                hbox.setLayoutX(10 + (pane.getPrefWidth() - hbox.getPrefWidth()) / 2);
-                tournaments.getChildren().add(pane);
-            }
+            MainGraphicController.displayTournaments(tL, visualizer, tournaments);
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
